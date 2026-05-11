2e5b4ddf07b5881da781418f6d5347d1845c11ca
anchal.koshta007@gmail.com <genious@genious.genious>
2026-05-11 22:52:13 +0530

    Making SQL Easy with Sequelize

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
-const db =  require('./utils/db-connection');
+const db = require('./utils/db-connection');
 const studentsRoute = require('./routes/studentsRoute');
 
 app.use(express.json());
 
-app.get('/', (req,res)=>{
+
+
+const studentModel = require('./models/students');
+
+app.get('/', (req, res) => {
     res.send('<h1> Student Management API</h1>');
 })
 
 app.use('/students', studentsRoute);
 
-app.listen(3000, () => console.log('Student Management API Project Online...'));
+db.sync({force: true}).then(() => {
+    
+    app.listen(3000, () => console.log('Student Management API Project Online...'));
+
+}).catch((err) => {
+    console.log(err);
+})
--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,30 +1,55 @@
-const mysql = require('mysql2');
+const { Sequelize } = require('sequelize');
 
-const connection= mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    password: '1234',
-    database: 'myFirstDb'
+
+ const sequelize = new Sequelize('myFirstDb', 'root', '1234', {
+  host: 'localhost',
+  dialect: 'mysql'
 });
 
-const connectionQuery = `CREATE TABLE IF NOT EXISTS STUDENTS (ID INT AUTO_INCREMENT PRIMARY KEY,
-NAME VARCHAR(20) NOT NULL, EMAIL VARCHAR(50) UNIQUE NOT NULL, AGE INT NOT NULL)`;
 
-connection.connect((err)=>{
-    if(err){
-        console.log(err.message);
-    }
-    console.log('Database Connected...');
+(async () => {try{
+    await sequelize.authenticate();
+    console.log("Connection to the database Has been created");
+    
+}catch(error){
+    console.log(error);
+}})();
 
-    connection.execute(connectionQuery, (err)=>{
-        if(err){
-            console.log(err.message);
-        }
-        console.log("Connection has been stablished Successfully...!");
-    })
-
-})
+module.exports = sequelize;
 
 
 
-module.exports = connection;
+
+
+
+
+// const mysql = require('mysql2');
+
+// const connection= mysql.createConnection({
+//     host: 'localhost',
+//     user: "root",
+//     password: '1234',
+//     database: 'myFirstDb'
+// });
+
+// const connectionQuery = `CREATE TABLE IF NOT EXISTS STUDENTS (ID INT AUTO_INCREMENT PRIMARY KEY,
+// NAME VARCHAR(20) NOT NULL, EMAIL VARCHAR(50) UNIQUE NOT NULL, AGE INT NOT NULL)`;
+
+// connection.connect((err)=>{
+//     if(err){
+//         console.log(err.message);
+//     }
+//     console.log('Database Connected...');
+
+//     connection.execute(connectionQuery, (err)=>{
+//         if(err){
+//             console.log(err.message);
+//         }
+//         console.log("Connection has been stablished Successfully...!");
+//     })
+
+// })
+
+
+
+// module.exports = connection;
