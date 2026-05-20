c8cd7beea6e7dc0fb2e64946ebf46343a35797b6
anchal.koshta007@gmail.com <genious@genious.genious>
2026-05-20 16:36:53 +0530

    Working cashfree code:

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,10 +1,9 @@
-const router = require('express').Router();
-
-const {getPaymentPage, processPayment, getPaymentStatus} = require('../controllers/paymentController');
+const express = require('express');
+const router = express.Router();
+const { getPaymentPage, processPayment, getPaymentStatus } = require('../controllers/paymentController');
 
 router.get('/', getPaymentPage);
-router.post ('/pay', processPayment);
-router.get('/payemnt-status/:paymentSessionId', getPaymentStatus);
-
+router.post('/pay', processPayment);
+router.get('/payment-status/:paymentSessionId', getPaymentStatus);
 
 module.exports = router;
